72dff1af38f74dc3111f2457db391b1ba3528cd9
Catalin Catta <catalincatta@yahoo.com>
2024-06-24 19:43:35 +0300

    added swagger

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  SwaggerModule.setup(
+    'api',
+    app,
+    SwaggerModule.createDocument(
+      app,
+      new DocumentBuilder()
+        .setTitle('Cats example')
+        .setDescription('The cats API description')
+        .setVersion('1.0')
+        .addTag('cats')
+        .build()
+    )
+  );
+
   await app.listen(3000);
 }
 
